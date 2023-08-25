ac41e39738f46a66b7eb6eee073ac7573821767a
qLoki <bobayan2005@gmail.com>
2023-08-25 17:00:46 +0300

    Backup1

---
+++ EFLibrary/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFLibrary
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+			
+		}
+	}
+}
---
+++ InventoryExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace InventoryExample
+{
+	internal class Program
+	{
+		private static IConfigurationRoot _configuration;
+		//private static DbContextOptionsBuilder<InventoryDbContext>_optionsBuilder;
+		static void Main(string[] args)
+		{
+			BuildOptions();
+		}
+		static void BuildOptions()
+		{
+			_configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+			//_optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+			//_optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+		}
+	}
+}
