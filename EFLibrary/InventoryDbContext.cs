f0dc5a15dbc03eba49b9df0be72c6cbf107caa7d
qLoki <bobayan2005@gmail.com>
2023-08-26 15:09:21 +0300

    Second pit-stop

--- EFLibrary/InventoryDbContext.cs
+++ EFLibrary/InventoryDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using InventoryModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFLibrary
 {
 	public class InventoryDbContext:DbContext
 	{
 		public DbSet<Item> Items { get; set; }
+		private static IConfigurationRoot _configuration;
 
 		public InventoryDbContext() { }
 		public InventoryDbContext(DbContextOptions options) : base(options)
@@ -16,7 +18,11 @@ namespace EFLibrary
 		{
 			if (!optionsBuilder.IsConfigured)
 			{
-				optionsBuilder.UseSqlServer("Data source=localhost;Initial Catalog=InventoryManagerDb;user=sa;password=loloshka228");
+				var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+				_configuration= builder.Build();
+				var cnstr = _configuration.GetConnectionString("InventoryManager");
+				optionsBuilder.UseSqlServer(cnstr);
+				//optionsBuilder.UseSqlServer("Data source=localhost;Initial Catalog=InventoryManagerDb;user=sa;password=loloshka228");
 			}
 		}
 	}
